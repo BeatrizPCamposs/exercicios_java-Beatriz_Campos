76c8273b275792b0b9010ed42c7ce69f5829f1be
BeatrizPCamposs <101807045+BeatrizPCamposs@users.noreply.github.com>
2022-03-25 16:58:17 -0300

    Exercicio 1

--- beatriz_campos4/src/beatriz_campos4/Beatriz_campos4.java
+++ beatriz_campos4/src/beatriz_campos4/Beatriz_campos4.java
@@ -37,15 +37,60 @@ public class Beatriz_campos4 {
         };
          
          System.out.println(unidade[Integer.parseInt((a))] [b]);
-     }
-       public static void um(){
-           Scanner leitor = new Scanner(System.in);
-           System.out.println("Digite um número");
-           String num = leitor.nextLine();
-           leitor.close();
-           
-           String 
-       }
+    public static void extenso(String a, int b){
+        String[][] unidade = {
+            {"","", "", ""},
+            {"onze","um", "dez", "cento"}, 
+            {"doze", "dois", "vinte", "duzentos"},
+            {"treze", "três", "trinta", "trezentos"}, 
+            {"quatorze", "quatro", "quarenta", "quatrocentos"},
+            {"quinze", "cinco", "cinquenta", "quinhentos"}, 
+            {"dezesseis", "seis", "sessenta", "seiscentos"}, 
+            {"dezesete", "sete", "setenta", "setecentos"}, 
+            {"dezoito", "oito", "oitenta", "oitocentos"}, 
+            {"dezenove", "nove", "noventa", "novecentos"} 
+        };
+        System.out.print(unidade[ Integer.parseInt((a)) ][b]); 
+    }
+    public static void um{
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite um número");
+        String num = leitor.nextLine();
+        leitor.close();
+        String cen = "",
+                dez = "",
+                uni = "";
+
+        uni += num.charAt(num.length() - 1);
+
+        if(num.length() > 2){ 
+            cen += num.charAt( num.length() - 3 ); 
+            if(num.charAt( num.length() - 3 ) != '0'){ 
+                extenso(cen, 3);
+                System.out.print(" e ");
+
+            }
+        }
+        if(num.length() > 1){ 
+            dez += num.charAt( num.length() - 2); 
+            if(num.charAt( num.length() - 2) != '0') { 
+                if(num.charAt( num.length() - 2) == '1') 
+                    extenso(uni, 0);
+                else{
+    
+                    extenso(dez, 2);
+                    System.out.print(" e ");
+                }
+            }
+        }
+        try {
+            if(num.charAt( num.length() - 2) != '1')
+            extenso(uni, 1);
+        } catch (Exception e) {
+            extenso(uni, 1);
+        }
+        
+    }
 }
     
 
